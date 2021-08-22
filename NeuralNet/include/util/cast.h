432a5e0ffcd4dabc377af1c6ed029f0d78a81c40
nabla27 <nabla3.1415@gmail.com>
2021-08-23 05:09:09 +0900

    update cast.h: add exception handing in typename_to_str().

--- NeuralNet/include/util/cast.h
+++ NeuralNet/include/util/cast.h
@@ -13,6 +13,8 @@ std::string typename_to_str()
 {
 	std::string type_info = typeid(T).name();
 	size_t i = type_info.find_last_of(" ") + 1;
+
+	if (i < 0) { return ""; }
 	return type_info.substr(i, type_info.length());
 }
 
