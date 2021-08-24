119133f163dbd624ec8f5d6a68d4ab12c40c0c2a
nabla27 <nabla3.1415@gmail.com>
2021-08-24 23:30:53 +0900

    test padding and pooling prehandling.

--- NeuralNet/main.cpp
+++ NeuralNet/main.cpp
@@ -27,35 +27,30 @@ int main()
 		vec::vector2d train_t = read_train_img.get_t();
 		vec::vector2d test_x = read_test_img.get_x();
 		vec::vector2d test_t = read_test_img.get_t();
-		{   //前処理
-			using namespace vec;
+
+		//前処理
+		{ using namespace vec;
 			size_t training_data_size = train_x.size();
 			size_t testing_data_size = test_x.size();
-			
+
 			for (size_t i = 0; i < training_data_size; ++i) 
 			{
 				vector2d _train_x = reshape_to<vector2d>(train_x[i], read_train_img.get_row(), read_train_img.get_col());
-				show(_train_x, "1"); _getch();  //__DEBUG__
-				_train_x = padding(_train_x, 1, 0);
-				show(_train_x, "2"); _getch();  //__DEBUG__
-				_train_x = pooling_max(_train_x, 5, 1);
-				show(_train_x, "3"); _getch();  //__DEBUG__
+				_train_x = padding(_train_x, 2, 0);
+				_train_x = pooling_average(_train_x, 5, 1);
 				train_x[i] = reshape_to<vector1d>(_train_x);
-				show(train_x[i], "4"); _getch(); //__DEBUG__
 				if (i < testing_data_size) 
 				{
 					vector2d _test_x = reshape_to<vector2d>(test_x[i], read_test_img.get_row(), read_test_img.get_col());
-					_test_x = padding(_test_x, 1, 0);
-					_test_x = pooling_max(_test_x, 5, 1);
+					_test_x = padding(_test_x, 2, 0);
+					_test_x = pooling_average(_test_x, 5, 1);
 					test_x[i] = reshape_to<vector1d>(_test_x);
 				}
-				vec::show(train_x[0], "train");
-				vec::show(test_x[0], "test");
-				(void)std::getchar();
 			}
+
+			train_x = train_x / 255;
+			test_x = test_x / 255;
 		}
-
-
 
 		/* ノード数の指定、重み・バイアスの初期化 */
 		nn::LayerSet layerset(train_x[0].size(), train_t[0].size());
@@ -68,7 +63,7 @@ int main()
 		nn::TrainCustom custom;
 		custom.acc_span = 200;
 		custom.batch_size = 100;
-		custom.dropout_ratio = 0.5;
+		custom.dropout_ratio = 0.1;
 		custom.learning_step = 50000;
 		custom.xmlout_inf = 0.98f;
 		custom.xml_span = 0;
@@ -83,7 +78,7 @@ int main()
 			> trainer(layerset);
 		trainer.set_TrainData(train_x, train_t);
 		trainer.set_TestData(test_x, test_t);
-		trainer.set_OutputPath("E:/MNIST/data/test/mnist");
+		trainer.set_OutputPath("E:/MNIST/data/08242316/mnist");
 		trainer.train(custom);
 
 
